077850e79e99866a880b250d311b09b7e6fdaa33
tianqili <tianqili@meituan.com>
2024-04-26 19:14:33 +0800

    feat: 更新配置

--- lua/community.lua
+++ lua/community.lua
@@ -5,18 +5,21 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
   -- { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   -- { import = "astrocommunity.colorscheme.sonokai" },
   -- { import = "astrocommunity.colorscheme.dracula-nvim" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.recipes.vscode-icons" },
   { import = "astrocommunity.completion.tabnine-nvim" },
   { import = "astrocommunity.completion.cmp-cmdline" },
   -- { import = "astrocommunity.editing-support.auto-save-nvim" },
-  { import = "astrocommunity.diagnostics.error-lens-nvim" },
+  -- { import = "astrocommunity.diagnostics.error-lens-nvim" },
 
   -- Node version not satisfy
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
